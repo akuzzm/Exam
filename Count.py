1c5f5851c726cc3c05623bffda96e6680211dbbd
akuzzm <akuzzm@gmail.com>
2017-09-15 20:00:03 +0300

    Create Count.py

---
+++ Count.py
@@ -0,0 +1,10 @@
+class Ten:
+
+  def summ(self, number_a, number_b):
+  
+     if number_a in range (0, 11) and number_b in range (0, 11):
+      result = number_a + number_b
+      return result
+      
+     else:
+      return -1
