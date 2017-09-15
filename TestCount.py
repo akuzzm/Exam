80c1e18b41587f17ba16c7b420bb293e1f590f60
akuzzm <akuzzm@gmail.com>
2017-09-15 23:26:59 +0300

    Update TestCount.py

--- TestCount.py
+++ TestCount.py
@@ -5,12 +5,51 @@ from Count import Ten
 class TestCount(unittest.TestCase):
   def setUp(self):
     self.res = Ten()
-    
+
   def test_positive(self):
     self.assertEqual(12, self.res.summ(6, 6))
+
+  def test_1(self):
+    self.assertEqual(20, self.res.summ(10, 10))
+
+  def test_2(self):
+    self.assertEqual(0, self.res.summ(0, 0))
+
+  def test_3(self):
+    self.assertEqual(10, self.res.summ(0, 10))
+
+  def test_4(self):
+    self.assertEqual(10, self.res.summ(10, 0))
+
+  def test_5(self):
+    self.assertEqual(-1, self.res.summ(-1, 11))
+
+  def test_6(self):
+    self.assertEqual(-1, self.res.summ(11, -1))
+
+  def test_7(self):
+    self.assertEqual(-1, self.res.summ(0, 11))
+
+  def test_8(self):
+    self.assertEqual(-1, self.res.summ(11, 0))
+
+  def test_9(self):
+    self.assertEqual(-1, self.res.summ(-1, 10))
     
-  def test_negative(self):
-    self.assertNotEqual(-1, self.res.summ(12, 15))
-    
-if__name__ == "__main__":
+  def test_10(self):
+    self.assertEqual(-1, self.res.summ(10, -1))
+
+  def test_11(self):
+    self.assertEqual(-1, self.res.summ(-5, -8))
+
+  def test_12(self):
+    self.assertEqual(-1, self.res.summ(20, 100))
+
+  def test_13(self):
+    self.assertEqual(-1, self.res.summ(20, -8))
+
+  def test_14(self):
+    self.assertEqual(-1, self.res.summ(-5, 100))
+
+if __name__ == "__main__":
   unittest.main()
