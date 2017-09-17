565d7829478c8a9d04393d94b49b25754b5e425a
akuzzm <akuzzm@gmail.com>
2017-09-17 23:23:22 +0300

    Update TestCount.py
    Correct!

--- TestCount.py
+++ TestCount.py
@@ -27,41 +27,5 @@ class TestCount(unittest.TestCase):
   def test_6(self):
     self.assertEqual(-1, self.res.summ(11, -1))
 
-  def test_7(self):
-    self.assertEqual(-1, self.res.summ(0, 11))
-
-  def test_8(self):
-    self.assertEqual(-1, self.res.summ(11, 0))
-
-  def test_9(self):
-    self.assertEqual(-1, self.res.summ(-1, 10))
-    
-  def test_10(self):
-    self.assertEqual(-1, self.res.summ(10, -1))
-
-  def test_11(self):
-    self.assertEqual(-1, self.res.summ(-5, -8))
-
-  def test_12(self):
-    self.assertEqual(-1, self.res.summ(20, 100))
-
-  def test_13(self):
-    self.assertEqual(-1, self.res.summ(20, -8))
-
-  def test_14(self):
-    self.assertEqual(-1, self.res.summ(-5, 100))
-    
-  def test_15(self):
-    self.assertEqual(-1, self.res.summ(-5, 6))
-    
-  def test_16(self):
-    self.assertEqual(-1, self.res.summ(6, -5))
-    
-  def test_17(self):
-    self.assertEqual(-1, self.res.summ(6, 100))
-    
-  def test_18(self):
-    self.assertEqual(-1, self.res.summ(100, 6))
-
 if __name__ == "__main__":
   unittest.main()
